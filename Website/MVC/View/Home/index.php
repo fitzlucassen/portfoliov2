827cc5b22004c9d0c5b73221664ad847d04ef66f
td <thibault.dulon@1000mercis.com>
2014-11-05 10:29:23 +0100

    modif required + db

--- Website/MVC/View/Home/index.php
+++ Website/MVC/View/Home/index.php
@@ -217,10 +217,10 @@
 
 		<div class="contactForm">
 			<form action="" method="POST">
-				<input type="email" name="email" placeholder="Votre adresse e-mail" />
+				<input type="email" name="email" placeholder="Votre adresse e-mail" required/>
 				<br/>
-				<input type="text" name="subject" placeholder="Sujet de votre demande" />
-				<textarea name="body" cols="10" rows="10" placeholder="Votre message"></textarea>
+				<input type="text" name="subject" placeholder="Sujet de votre demande" required/>
+				<textarea name="body" cols="10" rows="10" placeholder="Votre message" required></textarea>
 
 				<br/>
 				<input type="submit" value="Envoyer" />
